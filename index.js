39f3e005584856ce107ab279c872e5d691a7849a
Khoirunnisa Afifah <k.afis3@rocketmail.com>
2016-10-17 21:57:49 +0700

    fix promise

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const baseUrl = 'https://m.bnizona.com/index.php/category/index/promo';
 
 
 app.get('/', function(req, res) {
-	res.send('Try server:8000/all or server:8000/promo/{category}');
+	res.send('send GET request on {server}:8000/all to get all promo');
 });
 
 app.get('/all', function(req, res) {
 	promotion.findAll(baseUrl).then(function(result) {
-		res.status(200).send(result);
+		res.setHeader('Content-Type', 'application/json');
+		res.status(200).send(JSON.stringify(result), null, 4);
 	});
 });
 
 
 
 var server = app.listen(8000, function(){
-	var host = server.address().address;
 	var port = server.address().port;
-	console.log('App listening at http://%s:%s', host, port);
+	console.log('App listening at port %s', port);
 });
 
 
